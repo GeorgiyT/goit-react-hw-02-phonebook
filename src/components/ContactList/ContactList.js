2b903273984aa6caf51a9a8c3fa087906d66c806
Georgiy Tonkiy <georgiy.tonkiy@gmail.com>
2021-05-08 20:18:47 +0300

    Update ContactList.js

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -3,16 +3,13 @@ import PropTypes from "prop-types";
 import styles from "./ContactList.module.css";
 
 const ContactList = ({ contacts, deleteFunc }) => {
-  const handleClick = e => {
-    deleteFunc(e.target.name);
-  };
   return (
     <>
       <ul className={styles.contactList}>
         {contacts.map(el => (
           <li key={el.id}>
             {el.name}: {el.number}
-            <button type="button" name={el.id} onClick={handleClick}>
+            <button type="button" name={el.id} onClick={() => deleteFunc(el.id)}>
               Delete
             </button>
           </li>
